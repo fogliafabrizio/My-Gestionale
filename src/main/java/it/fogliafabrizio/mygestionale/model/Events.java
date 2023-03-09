e1e0ac0bac1101563ce6ebab5fb2facc9ad798e9
Fabrizio Foglia <fabryfoglia@me.com>
2023-03-09 16:38:04 +0100

    Creato Event e fermato

---
+++ src/main/java/it/fogliafabrizio/mygestionale/model/Events.java
@@ -0,0 +1,18 @@
+package it.fogliafabrizio.mygestionale.model;
+
+import java.util.List;
+
+public class Events {
+
+    private Long id;
+
+    private String eventName;
+
+    private String eventDescription;
+
+    private Long createdByUserId;
+
+    private List<Users> invitedUsers;
+
+
+}
